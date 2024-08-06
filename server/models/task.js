fb032382b4874458ae3ad31764f1e5778e69c2fa
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-08-06 17:36:40 +0530

    fixed some errors

--- server/models/task.js
+++ server/models/task.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import moment from "moment-timezone";
 
 const taskSchema = new mongoose.Schema({
   telegramId: { type: String, required: true },
@@ -6,6 +7,15 @@ const taskSchema = new mongoose.Schema({
   dueDate: { type: Date, required: true },
   notified: { type: Boolean, default: false },
 });
+
+// Middleware to adjust dueDate to Sri Lanka time before saving
+taskSchema.pre("save", function (next) {
+  if (this.dueDate) {
+    this.dueDate = moment.tz(this.dueDate, "Asia/Colombo").toDate();
+  }
+  next();
+});
+
 const Task = mongoose.model("Task", taskSchema);
 
 export default Task;
